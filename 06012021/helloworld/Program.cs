b75c9fcb8c42f44a04b8c2bd7e3840ea6f7abdc5
Tyler Mire <tyler.mire@revature.net>
2021-06-01 14:47:30 -0700

    Now using the correct branch

--- 06012021/helloworld/Program.cs
+++ 06012021/helloworld/Program.cs
@@ -8,13 +8,15 @@ namespace helloworld
         {
             Console.WriteLine("Hello World!");
 
+            //Prompt user for age
             Console.WriteLine("Please enter age:");
-            string userAge = Console.ReadLine();
+            int userAge = Int32.Parse(Console.ReadLine());
 
+            //Prompt user for name
             Console.WriteLine("Please enter name:");
             string userName = Console.ReadLine();
 
-
+            //Print to console using string interpolation
             Console.WriteLine($"Your name is {userName} and you are {userAge} year(s) old");
         }
     }
