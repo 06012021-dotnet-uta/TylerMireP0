6cd5a6ec6dec863a3b872700300b37a89137aba9
Tyler Mire <tyler.mire@revature.net>
2021-06-01 14:38:44 -0700

    Completed Assignment

---
+++ 06012021/helloworld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Please enter age:");
+            string userAge = Console.ReadLine();
+
+            Console.WriteLine("Please enter name:");
+            string userName = Console.ReadLine();
+
+
+            Console.WriteLine($"Your name is {userName} and you are {userAge} year(s) old");
+        }
+    }
+}
