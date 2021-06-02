02e0d290f6618fd4d36b3aa0f5e0cc1ff7254b26
Tyler Mire <tyler.mire@revature.net>
2021-06-02 08:54:58 -0700

    Added comments

--- 06012021/helloworld/Program.cs
+++ 06012021/helloworld/Program.cs
@@ -6,6 +6,7 @@ namespace helloworld
     {
         static void Main(string[] args)
         {
+            //Hi!
             Console.WriteLine("Hello World!");
 
             //Prompt user for age
