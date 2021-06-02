89340d2bfa8ff72166e274064c99f8ed3dbd9f7f
Tyler Mire <tyler.mire@revature.net>
2021-06-02 15:37:49 -0700

    Completed simpleArraySum method

--- Assignments/HackerRankChallengeWeek1-1/Program.cs
+++ Assignments/HackerRankChallengeWeek1-1/Program.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System;
 
 namespace HackerRankChallengeWeek1_1
 {
+
+    class Result
+    {
+
+        /*
+        * Complete the 'simpleArraySum' function below.
+        *
+        * The function is expected to return an INTEGER.
+        * The function accepts INTEGER_ARRAY ar as parameter.
+        */
+
+        public static int simpleArraySum(List<int> ar)
+        {
+            int sum = 0;
+            for(int i = 0; i < ar.Count; i++)
+            {
+                sum += ar[i];
+            }
+            return sum;
+        }
+
+    }
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+            int result = Result.simpleArraySum(ar);
+
+            using(TextWriter textWriter = new StreamWriter(Directory.GetCurrentDirectory() + @"\Output.txt", true))
+            {
+                textWriter.WriteLine(result);
+            }
         }
     }
 }
