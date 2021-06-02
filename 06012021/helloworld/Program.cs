5a6dd5750250906cae1eb820baffc3cd5f1d938b
Tyler Mire <tyler.mire@revature.net>
2021-06-02 07:11:40 -0700

    added age verification

--- 06012021/helloworld/Program.cs
+++ 06012021/helloworld/Program.cs
@@ -12,6 +12,13 @@ namespace helloworld
             Console.WriteLine("Please enter age:");
             int userAge = Int32.Parse(Console.ReadLine());
 
+            //Verify that age is correct
+            while(userAge > 120 || userAge < 0)
+            {
+                Console.WriteLine("Please enter valid age: ");
+                userAge = Int32.Parse(Console.ReadLine());
+            }
+
             //Prompt user for name
             Console.WriteLine("Please enter name:");
             string userName = Console.ReadLine();
