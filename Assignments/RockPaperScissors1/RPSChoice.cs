fd7152be4b82e104307f538d481b1501cc8a4fdc
Tyler Mire <tyler.mire@revature.net>
2021-06-03 10:10:13 -0700

    Updated RPS functions

--- Assignments/RockPaperScissors1/RPSChoice.cs
+++ Assignments/RockPaperScissors1/RPSChoice.cs
@@ -1,17 +1,3 @@
-/*
-    Assignment Goals RPS V1:
-    1. Start game - intro
-    2. Prompt for choice           - x3
-    3. Press enter                 - x3
-    4. Print result of each round  - x3
-    5. Print the winner
-    6. Prompt user for additional game
-    7. Get Name from player and print to console for victory
-    8. Track wins and print results screen 
-
-    --Also do the coding challange for this week--
-*/
-
 namespace RockPaperScissors1
 {
     partial class Program
